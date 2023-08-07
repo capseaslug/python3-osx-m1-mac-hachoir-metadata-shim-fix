a9f306495f2c926cb76fe9b847551619a5f87e52
capseaslug <todelldesign@gmail.com>
2023-08-06 17:21:04 -0700

    Update metadata-shim.py

--- metadata-shim.py
+++ metadata-shim.py
@@ -1,1 +1,9 @@
+import hachoir.core
 
+def compat_version(version):
+    """Return a compatible version string for hachoir-core==1.3.3."""
+    if version.startswith("3.2."):
+        return "1.3.3"
+    return version
+
+hachoir.core.VERSION = compat_version(hachoir.core.VERSION)
