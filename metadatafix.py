5b9d677b6d22c53241f95e0cc666dfb928e0ad6a
capseaslug <todelldesign@gmail.com>
2023-08-06 18:39:37 -0700

    Update metadatafix.py

--- metadatafix.py
+++ metadatafix.py
@@ -5,6 +5,7 @@ import gzip
 import re
 import pip
 
+
 def is_apple_silicon_mac():
     """Return True if the current machine is an Apple Silicon Mac."""
     return platform.machine().startswith("arm64")
@@ -17,47 +18,33 @@ def install_hachoir():
         pip.main(["install", "hachoir-parser"])
 
 def modify_metadata_requirements():
-    """Modify the metadata package requirements to be compatible with hachoir-core==1.3.3."""
+    """Download the metadata package, extract the metadata, modify it to include hachoir-core==1.3.3, and then save the modified metadata."""
+
+    url = "https://files.pythonhosted.org/packages/8a/2b/3982e589808af51d6e8d3f7ebb6c540005ad3466d1e49"
 
     # Download the metadata package
-    url = "https://files.pythonhosted.org/packages/8a/2b/3982e589808af51d6e8d3f7ebb6c540005ad3466d1e4939e57d25cd1d3c0/metadata-0.2.tar.gz"
-    filename = "metadata-0.2.tar.gz"
-
     with requests.get(url, stream=True) as r:
-        with open(filename, "wb") as f:
+        with open("metadata.tar.gz", "wb") as f:
             for chunk in r.iter_content(chunk_size=1024):
                 f.write(chunk)
 
-    # Check if the file exists
-    if not os.path.exists(filename):
-        raise FileNotFoundError(f"File '{filename}' does not exist.")
+    # Extract the metadata from the package
+    with gzip.open("metadata.tar.gz", "rb") as f:
+        metadata = f.read().decode("utf-8")
 
-    # Uncompress the metadata package
-    os.system("gzip -d %s" % filename)
+    # Modify the metadata to include hachoir-core==1.3.3
+    print("Replacing hachoir-core version...")
+    metadata = re.sub(r"hachoir-core==\d.\d.\d", "hachoir-core==1.3.3", metadata)
 
-    # Search for the requirements file
-    for root, directories, files in os.walk("metadata-0.2"):
-        for file in files:
-            if re.match("requirements\.txt", file):
-                filename = os.path.join(root, file)
-                break
+    # Save the modified metadata to a new file
+    with open("metadata.tar.gz", "wb") as f:
+        f.write(metadata.encode("utf-8"))
 
-    # Modify the requirements file
-    with open(filename, "r") as f:
-        requirements = f.readlines()
+    # Install the modified metadata
+    print("Installing modified metadata...")
+    pip.main(["install", "-r", "metadata.tar.gz"])
 
-    for index, requirement in enumerate(requirements):
-        if requirement.startswith("hachoir-core=="):
-            requirements[index] = "hachoir==1.3.3"
-
-    with open(filename, "w") as f:
-        f.writelines(requirements)
-
-def update_metadata_package():
-    """Update the metadata package to be compatible with hachoir."""
-
-    # Install the modified requirements file
-    os.system("cd metadata-0.2 && pip install -r requirements.txt")
+    print("Success! The metadata requirements have been modified and installed.")
 
 def main():
     """Check if the current machine is an Apple Silicon Mac.
@@ -72,7 +59,6 @@ def main():
 
     install_hachoir()
     modify_metadata_requirements()
-    update_metadata_package()
 
 if __name__ == "__main__":
     main()
